53653eacd110d928813308b00fa52865bbbf0fe3
Adrian Garza <adriangarza236@yahoo.com>
2025-02-19 08:39:16 -0600

    completed chatterbox lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,68 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.all()]
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            messages,
+            200
+        )
+        return response
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.get_json().get("body"),
+            username=request.get_json().get("username"),
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            message_dict,
+            201
+        )
+        return response 
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if request.method == 'PATCH':
+        updated_message = Message(
+            body=request.get_json().get("body")
+        )
+    
+        db.session.add(updated_message)
+        db.session.commit()
+
+        updated_message_dict = updated_message.to_dict()
+
+        response = make_response(
+            updated_message_dict,
+            201
+        )
+        return response
+    
+    elif request.method == 'DELETE':
+
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_succesful": True,
+            "message": "Message deleted"
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+        return response 
 
 if __name__ == '__main__':
     app.run(port=5555)
